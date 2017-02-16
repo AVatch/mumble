27f25c7bb0196e1bc09a9e590a6db24ae0d6ec82
Adrian Vatchinsky <avatchinsky@dhcp-ccc-9033.redrover.cornell.edu>
2017-02-16 16:28:47 -0500

    setup .env

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
-from os import path
+import os
+from os.path import join, dirname, abspath
+
+from dotenv import load_dotenv
+
+# load dotenv
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # App details
-BASE_DIRECTORY = path.abspath(path.dirname(__file__))
+BASE_DIRECTORY = abspath(dirname(__file__))
 DEBUG = True
-SECRET_KEY = 'keep_it_like_a_secret'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Database details
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = '{0}{1}'.format('sqlite:///',
-                                          path.join(BASE_DIRECTORY, 'app.db'))
+                                          join(BASE_DIRECTORY, 'app.db'))
